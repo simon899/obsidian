4a72af7cc737d22fbd38f41af95077638cd3c071
simon899 <wangxing891022@gmail.com>
2026-06-11 14:15:33 +0800

    auto

--- autosync.ps1
+++ autosync.ps1
@@ -1,5 +1,8 @@
 ﻿$env:Path = "C:\Program Files\Git\cmd;$env:Path"
 cd "C:\obsidian-vault"
+
+# 先拉取远程更新，再提交本地更改，最后推送
+git pull --rebase --autostash 2>&1
 $result = git add -A 2>&1
 $result = git commit -m "auto" 2>&1
 if ($LASTEXITCODE -eq 0) {
